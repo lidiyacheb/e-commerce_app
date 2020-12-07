2ac916e77a6b927ffdfe8809ee8c2bbb6f74c4f5
lidiyacheb <lidia.saulite@gmail.com>
2020-12-07 11:36:36 +0100

    add connection to db

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 var pg = require('pg');
+var morgan = require('morgan')
+
+app.use(morgan("common"));
+app.use(express.json());
+
+
+var pool = new pg.Pool({
+    user: 'ayfojtcc',
+    host: 'suleiman.db.elephantsql.com',
+    database: 'ayfojtcc',
+    password: 'z6QtwdqX-mZyHNaQ-R6YywxlOAUK41ju',
+    port: 5432
+  });
+
+console.log(pool);
+
+
+
 const PORT = process.env.PORT || '4000';
 
 app.listen(PORT, () => {
